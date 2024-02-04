2ff8068fee75f3c86bec2ce95622aa2da82a70c0
Maksym Kurochka <maksym.kurochka@gmail.com>
2024-02-04 11:15:15 +0100

    Function that selects data for Manhattan only

--- Code/Filter_station_only_Manhattan.py
+++ Code/Filter_station_only_Manhattan.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv('Datasets/2013-07_Citi_Bike_trip_dane_podstawowe_part_2.csv')
+def filter_func():   
+    df = pd.read_csv('Datasets/2013-07_Citi_Bike_trip_dane_podstawowe_part_2.csv')
 
-filt = list(pd.read_csv("Datasets/Manhattan_stations (2023ukr).csv")['station id'])
+    filt = list(pd.read_csv("Datasets/Manhattan_stations (2023ukr).csv")['station id'])
 
-df2 = pd.DataFrame(columns=df.columns)
+    df2 = pd.DataFrame(columns=df.columns)
 
-for i in range(df.shape[0]):
-    if df.iloc[i]['start station id'] in filt and df.iloc[i]['end station id'] in filt:
-        p = pd.Series(list(df.iloc[i]), index=df2.columns)
-        df2.loc[df2.shape[0]] = p
+    for i in range(df.shape[0]):
+        if df.iloc[i]['start station id'] in filt and df.iloc[i]['end station id'] in filt:
+            p = pd.Series(list(df.iloc[i]), index=df2.columns)
+            df2.loc[df2.shape[0]] = p
 
-df2.to_csv('Datasets/Only_Manhattan')
+    df2.to_csv('Datasets/Only_Manhattan')
+
+filter_func()
